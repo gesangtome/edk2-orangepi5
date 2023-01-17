d3777332e9b71d36b54c2ef95fe251824dfc502f
gesangtome <gesangtome@foxmail.com>
2023-01-17 20:56:55 +0800

    Build idblock.bin for SPI Image
    Add build tools and steps.
    
    Signed-off-by: gesangtome <gesangtome@foxmail.com>

--- make.sh
+++ make.sh
@@ -8,3 +8,8 @@ export GCC5_AARCH64_PREFIX=aarch64-none-linux-gnu-
 . edk2/edksetup.sh
 build -a AARCH64 -t GCC5 -p Platform/Rockchip/RK3588/RK3588.dsc -D ROCKCHIP_PCIE30 -D ROCKCHIP_VOPEN -D ROCKCHIP_ACPIEN
 
+echo "Build idblock Image ..."
+./rkbin/tools/x86_64/mkimage \
+    -n rk3588 -T rksd \
+    -d rkbin/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.08.bin:rkbin/rk3588_spl_v1.11.bin \
+    idblock.bin
